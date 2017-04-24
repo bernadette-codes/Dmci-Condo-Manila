24c74b650ce3d33fd3cc70effb02d794878012db
Bernadette <bernadette@englemaninvest.com>
2017-04-23 22:36:53 -0400

    Added Router

--- app/components/main.js
+++ app/components/main.js
@@ -1,18 +1,35 @@
 var React = require('react'),
-    Landing = require('./landing'),
+    ReactRouter = require('react-router-dom'),
+    Router = ReactRouter.BrowserRouter,
+    Route = ReactRouter.Route,
+    Switch = ReactRouter.Switch,
     NavBar = require('./nav'),
-    ContactModal = require('./contactModal');
+    Landing = require('./landing'),
+    ContactModal = require('./contactModal'),
+    CitySection = require('./citySection'),
+    GuideSection = require('./guideSection'),
+    AboutSection = require('./aboutSection');
 
 class Main extends React.Component {
     render() {
         return (
-            <div>
-                <Landing />
+            <Router>
+                <div>
+                    <NavBar />
 
-                <NavBar />
+                    <ContactModal />
 
-                <ContactModal />
-            </div>
+                    <Switch>
+                        <Route exact path='/' component={Landing} />
+                        <Route exact path='/Properties' component={CitySection} />
+                        <Route exact path='/Buyersguide' component={GuideSection} />
+                        <Route exact  path='/About' component={AboutSection} />
+                        <Route render={ function () {
+                            return <p>Not Found</p>
+                        }} />
+                    </Switch>
+                </div>
+            </Router>
         )
     }
 }
