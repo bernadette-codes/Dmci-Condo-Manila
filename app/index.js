fafee64fb016f9dffc6656fc294a07e130de57ff
Bernadette <bernadette@englemaninvest.com>
2017-04-23 20:32:20 -0400

    Removed components

--- app/index.js
+++ app/index.js
@@ -1,29 +1,9 @@
 var React = require('react'),
     ReactDOM = require('react-dom'),
-    Landing = require('./components/landing'),
-    CitySection = require('./components/citySection'),
-    CityProp = require('./components/cityProp'),
-    GuideSection = require('./components/guideSection'),
-    AboutSection = require('./components/aboutSection'),
-    ContactBox = require('./components/contactBox');
-require('./index.css');
+    Main = require('./components/main');
+require('./css/styles.css');
 
 
 ReactDOM.render(
-    <Landing />, document.getElementById('first-slider')
-);
-ReactDOM.render(
-    <CitySection />, document.getElementById('propertiesSection')
-);
-ReactDOM.render(
-    <CityProp />, document.getElementById('cityprop')
-);
-ReactDOM.render(
-    <GuideSection />, document.getElementById('buyersGuideSection')
-);
-ReactDOM.render(
-    <AboutSection />, document.getElementById('aboutSection')
-);
-ReactDOM.render(
-    <ContactBox />, document.getElementById('contactBox')
+    <Main />, document.getElementById('mainContent')
 );
