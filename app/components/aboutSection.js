23db313119b61f73844a62e4c0b0b15b1fdf4989
Bernadette <bernadette@englemaninvest.com>
2017-04-25 21:18:47 -0400

    Added footer component

--- app/components/aboutSection.js
+++ app/components/aboutSection.js
@@ -1,10 +1,11 @@
-var React = require('react'),
-    ContactBox = require('./contactBox');
+const React = require('react'),
+    ContactBox = require('./ContactBox'),
+    DMCILogo = require('./DmciLogo'),
+    Footer = require('./Footer');
 
 class AboutSection extends React.Component {
     render() {
-        var section = ['PROPERTIES', 'BUYERS GUIDE', 'About DMCI'],
-            features = [
+        const features = [
             {
                 "feature": "QUALITY",
                 "description": "DMCI Homes provides one (1) year quality warranty that covers repairs, free of charge, on any workmanship defects of the unit. Fast and reliable, the company's construction of the residential buildings and turnover of units are within 6 to 12 months only.",
@@ -28,41 +29,42 @@ class AboutSection extends React.Component {
         ];
 
         return (
-            <section id="aboutSection" className="container-fluid">
-                <ContactBox />
+            <div>
+                <section id="aboutSection" className="container-fluid">
+                    <ContactBox />
 
-                <div className="dmciSection">
-                    <div className="dmciLogo">
-                        <img src="app/img/dmci-logo1.jpg" alt="DMCI Logo" className="img-responsive" />
+                    <div className="dmciSection">
+                        <DMCILogo />
+
+                        <h2 className="text-left" style={{ background: "url('app/img/icons/about.png') 10px 15px/25px no-repeat padding-box" }}>About DMCI</h2>
+                        <hr />
+
+                        <img src="app/img/condoImg/resort.jpg" alt="Pool" className="img-responsive designImg" />
+                        <img src="app/img/condoImg/playarea.jpg" alt="Play Area" className="img-responsive designImg" />
+                        <img src="app/img/condoImg/gym.jpg" alt="Play Area" className="img-responsive designImg" />
+
+                        <div>
+                            {features.map(function(list) {
+                                return (
+                                    <div className="info" key={list.feature}>
+                                        <h3>{list.feature}</h3>
+                                        <p>{list.description}</p>
+                                        <ul>
+                                            {list.more.map(function(item) {
+                                                return (
+                                                    <li key={item}>{item}</li>
+                                                );
+                                            })}
+                                        </ul>
+                                    </div>
+                                );
+                            })}
+                        </div>
                     </div>
+                </section>
 
-                    <h2 className="text-left" style={{ background: "url('app/img/icons/about.png') 10px 15px/25px no-repeat padding-box" }}>{section[2]}</h2>
-                    <hr />
-
-                    <img src="app/img/condoImg/resort.jpg" alt="Pool" className="img-responsive designImg" />
-                    <img src="app/img/condoImg/playarea.jpg" alt="Play Area" className="img-responsive designImg" />
-                    <img src="app/img/condoImg/gym.jpg" alt="Play Area" className="img-responsive designImg" />
-
-                    <div>
-                        {features.map(function(list) {
-                            return (
-                                <div className="info" key={list.feature}>
-                                    <h3>{list.feature}</h3>
-                                    <p>{list.description}</p>
-                                    <ul>
-                                        {list.more.map(function(item) {
-                                            return (
-                                                <li key={item}>{item}</li>
-                                            );
-                                        })}
-                                    </ul>
-                                </div>
-                            );
-                        })}
-                    </div>
-
-                </div>
-            </section>
+                <Footer />
+            </div>
         )
     }
 }
