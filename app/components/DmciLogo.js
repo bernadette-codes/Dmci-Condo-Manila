c75f9e649a62c484b8456c1b6866febe5b87ab01
Bernadette <bernadette@englemaninvest.com>
2017-04-26 21:45:46 -0400

    Create DmciLogo.js

---
+++ app/components/DmciLogo.js
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class DMCILogo extends React.Component {
+    render() {
+        return (
+            <div className="dmciLogo">
+                <img src="app/img/dmci-logo1.jpg" alt="DMCI Logo" className="img-responsive" />
+            </div>
+        )
+    }
+}
+
+module.exports = DMCILogo;
