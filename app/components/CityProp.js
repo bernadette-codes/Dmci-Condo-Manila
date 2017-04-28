563da112cda25c29a2502435f212089c2362a681
Bernadette <bernadette@englemaninvest.com>
2017-04-27 23:26:14 -0400

    Corrected link + added Prisma

--- app/components/CityProp.js
+++ app/components/CityProp.js
@@ -1,5 +1,4 @@
 const React = require('react'),
-    Link = require('react-router-dom').Link,
     // Modules
     ContactBox = require('./Contact/ContactBox'),
     DMCILogo = require('./DmciLogo');
@@ -19,7 +18,7 @@ class CityProp extends React.Component {
                     "title": "Ready for Occupancy",
                     "properties": [{
                         "propName": "Maricielo Villas",
-                        "link": "properties/MaricieloVillasLasPinas.html#mainSection",
+                        "link": "app/properties/MaricieloVillasLasPinas.html#mainSection",
                         "namePhoto": "app/img/propertiesName/lasPinas-MaricieloVillas.jpg",
                         "mapPhoto": "app/img/propertiesMap/lasPinas-MaricieloVillas.jpg",
                         "address": "Casimiro Avenue, Las Piñas"
@@ -38,7 +37,7 @@ class CityProp extends React.Component {
                     "title": "Under Construction",
                     "properties": [{
                         "propName": "Brio Tower",
-                        "link": "properties/BrioTowerMakati.html#mainSection",
+                        "link": "app/properties/BrioTowerMakati.html#mainSection",
                         "namePhoto": "app/img/propertiesName/makati-BrioTower.jpg",
                         "mapPhoto": "app/img/propertiesMap/makati-BrioTower.jpg",
                         "address": "Guadalupe Viejo, Makati City"
@@ -65,19 +64,19 @@ class CityProp extends React.Component {
                         "title": "Ready for Occupancy",
                         "properties": [{
                             "propName": "Dansalan Gardens",
-                            "link": "properties/DansalanGardensCondoMandaluyong.html#mainSection",
+                            "link": "app/properties/DansalanGardensCondoMandaluyong.html#mainSection",
                             "namePhoto": "app/img/propertiesName/mandaluyong-DansalanGardens.jpg",
                             "mapPhoto": "app/img/propertiesMap/mandaluyong-DansalanGardens.jpg",
                             "address": "M. Vicente St., Mandaluyong City"
                         }, {
                             "propName": "Flair Towers",
-                            "link": "properties/FlairTowersMandaluyong.html#mainSection",
+                            "link": "app/properties/FlairTowersMandaluyong.html#mainSection",
                             "namePhoto": "app/img/propertiesName/mandaluyong-FlairTowers.jpg",
                             "mapPhoto": "app/img/propertiesMap/mandaluyong-FlairTowers.jpg",
                             "address": "Reliance St. Corner Pines Highway Hills, Mandaluyong City"
                         }, {
                             "propName": "Tivoli Garden",
-                            "link": "properties/TivoliGardenResidencesMandaluyong.html#mainSection",
+                            "link": "app/properties/TivoliGardenResidencesMandaluyong.html#mainSection",
                             "namePhoto": "app/img/propertiesName/mandaluyong-TivoliGarden.jpg",
                             "mapPhoto": "app/img/propertiesMap/mandaluyong-TivoliGardenResidences.jpg",
                             "address": "Coronoado Street, Brgy. Hulo, Mandaluyong City"
@@ -99,7 +98,7 @@ class CityProp extends React.Component {
                     "title": "Ready for Occupancy",
                     "properties": [{
                         "propName": "Rhapsody Residences",
-                        "link": "properties/RhapsodyResidencesMuntinlupa.html#mainSection",
+                        "link": "app/properties/RhapsodyResidencesMuntinlupa.html#mainSection",
                         "namePhoto": "app/img/propertiesName/muntinlupa-Rhapsody.jpg",
                         "mapPhoto": "app/img/propertiesMap/muntinlupa-Rhapsody.jpg",
                         "address": "East Service Road, Barrio Cupang, Muntinlupa City"
@@ -118,13 +117,13 @@ class CityProp extends React.Component {
                     "title": "Under Construction",
                     "properties": [{
                         "propName": "Calathea Place",
-                        "link": "properties/CalatheaPlaceParanaque.html#mainSection",
+                        "link": "app/properties/CalatheaPlaceParanaque.html#mainSection",
                         "namePhoto": "app/img/propertiesName/paranaque-CalatheaPlace.jpg",
                         "mapPhoto": "app/img/propertiesMap/paranaque-CalatheaPlace.jpg",
                         "address": "Dr. A. Santos Ave. Parañaque City"
                     }, {
                         "propName": "Oak Harbor Residences",
-                        "link": "properties/OakHarborResidencesParanaque.html#mainSection",
+                        "link": "app/properties/OakHarborResidencesParanaque.html#mainSection",
                         "namePhoto": "app/img/propertiesName/paranaque-OakHarbor.jpg",
                         "mapPhoto": "app/img/propertiesMap/paranaque-OakHarborResidences.jpg",
                         "address": "Jackson Ave. Asiaworld City, Brgy. Don Galo, Parañaque City"
@@ -136,25 +135,25 @@ class CityProp extends React.Component {
                     "title": "Ready for Occupancy",
                     "properties": [{
                         "propName": "Arista Place",
-                        "link": "properties/AristaPlaceParanaque.html#mainSection",
+                        "link": "app/properties/AristaPlaceParanaque.html#mainSection",
                         "namePhoto": "app/img/propertiesName/paranaque-AristaPlace.jpg",
                         "mapPhoto": "app/img/propertiesMap/paranaque-AristaPlace.jpg",
                         "address": "JP Rizal St., Brgy. Sto. Niño, Parañaque City"
                     }, {
                         "propName": "Asteria Residences",
-                        "link": "properties/AsteriaResidencesParanaque.html#mainSection",
+                        "link": "app/properties/AsteriaResidencesParanaque.html#mainSection",
                         "namePhoto": "app/img/propertiesName/paranaque-AsteriaResidences.jpg",
                         "mapPhoto": "app/img/propertiesMap/paranaque-AsteriaResidences.jpg",
                         "address": "San Pedro Street , San Antonio Valley 2 Brgy San Isidro, Sucat, Parañaque City"
                     }, {
                         "propName": "Raya Garden",
-                        "link": "properties/RayaGardenCondoParanaque.html#mainSection",
+                        "link": "app/properties/RayaGardenCondoParanaque.html#mainSection",
                         "namePhoto": "app/img/propertiesName/paranaque-RayaGarden.jpg",
                         "mapPhoto": "app/img/propertiesMap/paranaque-RayaGarden.jpg",
                         "address": "West Service Road, Merville, Parañaque City"
                     }, {
                         "propName": "Siena Park Residences",
-                        "link": "properties/SienaParkResidencesParanaque.html#mainSection",
+                        "link": "app/properties/SienaParkResidencesParanaque.html#mainSection",
                         "namePhoto": "app/img/propertiesName/paranaque-SienaPark.jpg",
                         "mapPhoto": "app/img/propertiesMap/paranaque-SienaParkResidences.jpg",
                         "address": "West Service Road, Sun Valley, Bicutan, Parañaque City"
@@ -173,7 +172,7 @@ class CityProp extends React.Component {
                     "title": "Under Construction",
                     "properties": [{
                         "propName": "Fairway Terraces",
-                        "link": "properties/FairwayTerracesPasay.html#mainSection",
+                        "link": "app/properties/FairwayTerracesPasay.html#mainSection",
                         "namePhoto": "app/img/propertiesName/pasay-FairwayTerraces.jpg",
                         "mapPhoto": "app/img/propertiesMap/pasay-FairwayTerraces.jpg",
                         "address": "Villamor Airbase, Pasay City"
@@ -185,7 +184,7 @@ class CityProp extends React.Component {
                     "title": "Ready for Occupancy",
                     "properties": [{
                         "propName": "La Verti Residences",
-                        "link": "properties/LaVertiResidencesPasay.html#mainSection",
+                        "link": "app/properties/LaVertiResidencesPasay.html#mainSection",
                         "namePhoto": "app/img/propertiesName/pasay-LaVerti.jpg",
                         "mapPhoto": "app/img/propertiesMap/pasay-LaVertiResidences.jpg",
                         "address": "Taft Avenue, Pasay City"
@@ -204,25 +203,31 @@ class CityProp extends React.Component {
                     "title": "Under Construction",
                     "properties": [{
                         "propName": "Brixton Place",
-                        "link": "properties/BrixtonPlacePasig.html#mainSection",
+                        "link": "app/properties/BrixtonPlacePasig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/pasig-BrixtonPlace.jpg",
                         "mapPhoto": "app/img/propertiesMap/pasig-BrixtonPlace.jpg",
                         "address": "27 Brixton St., Kapitolyo, Pasig City"
                     }, {
                         "propName": "Lumiere Residences",
-                        "link": "properties/LumiereResidencesPasig.html#mainSection",
+                        "link": "app/properties/LumiereResidencesPasig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/pasig-LumiereResidences.jpg",
                         "mapPhoto": "app/img/propertiesMap/pasig-LumiereResidences.jpg",
                         "address": "Pasig Blvd., cor. Shaw Blvd., Pasig City"
                     }, {
                         "propName": "Mirea Residences",
-                        "link": "properties/MireaResidencesPasig.html#mainSection",
+                        "link": "app/properties/MireaResidencesPasig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/pasig-MireaResidences.jpg",
                         "mapPhoto": "app/img/propertiesMap/pasig-MireaResidences.jpg",
-                        "address": "Amang Rodriquez Ave., Brgy Santolan, Pasig Cit"
+                        "address": "Amang Rodriquez Ave., Brgy Santolan, Pasig City"
+                    }, {
+                        "propName": "Prisma Residences",
+                        "link": "app/properties/PrismaResidencesPasig.html#mainSection",
+                        "namePhoto": "app/img/propertiesName/pasig-PrismaResidences.jpg",
+                        "mapPhoto": "app/img/propertiesMap/pasig-PrismaResidences.jpg",
+                        "address": "Pasig Boulevard, Brgy. Bagong Ilog, 1600"
                     }, {
                         "propName": "Sheridan Towers",
-                        "link": "properties/SheridanTowersPasig.html#mainSection",
+                        "link": "app/properties/SheridanTowersPasig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/pasig-SheridanTowers.jpg",
                         "mapPhoto": "app/img/propertiesMap/pasig-SheridanTowers.jpg",
                         "address": "Sheridan St., Pasig City"
@@ -234,13 +239,13 @@ class CityProp extends React.Component {
                     "title": "Ready for Occupancy",
                     "properties": [{
                         "propName": "East Raya Gardens",
-                        "link": "properties/EastRayaGardensPasig.html#mainSection",
+                        "link": "app/properties/EastRayaGardensPasig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/pasig-EastRayaGardens.jpg",
                         "mapPhoto": "app/img/propertiesMap/pasig-EastRayaGardens.jpg",
                         "address": "One Mercedes Avenue, Corner Luis St. Brgy. Mercedes, Pasig City"
                     }, {
                         "propName": "Levina Place",
-                        "link": "properties/LevinaPlacePasig.html#mainSection",
+                        "link": "app/properties/LevinaPlacePasig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/pasig-LevinaPlace.jpg",
                         "mapPhoto": "app/img/propertiesMap/pasig-LevinaPlace.jpg",
                         "address": "Jenny's Ave., Pasig City"
@@ -259,19 +264,19 @@ class CityProp extends React.Component {
                     "title": "Under Construction",
                     "properties": [{
                         "propName": "Maple Place",
-                        "link": "properties/MaplePlaceTaguig.html#mainSection",
+                        "link": "app/properties/MaplePlaceTaguig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/taguig-MaplePlace.jpg",
                         "mapPhoto": "app/img/propertiesMap/taguig-MaplePlace.jpg",
                         "address": "Acacia Estates, Taguig City"
                     }, {
                         "propName": "Ivory Wood",
-                        "link": "properties/IvoryWoodTaguig.html#mainSection",
+                        "link": "app/properties/IvoryWoodTaguig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/taguig-Ivorywood.jpg",
                         "mapPhoto": "app/img/propertiesMap/taguig-IvoryWood.jpg",
                         "address": "Acacia Estates, Taguig City"
                     }, {
                         "propName": "The Birchwood",
-                        "link": "properties/TheBirchwoodTaguig.html#mainSection",
+                        "link": "app/properties/TheBirchwoodTaguig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/taguig-TheBirchwood.jpg",
                         "mapPhoto": "app/img/propertiesMap/taguig-TheBirchwood.jpg",
                         "address": "Acacia Estates, Taguig City"
@@ -283,19 +288,19 @@ class CityProp extends React.Component {
                     "title": "Ready for Occupancy",
                     "properties": [{
                         "propName": "Cedar Crest",
-                        "link": "properties/CedarCrestTaguig.html#mainSection",
+                        "link": "app/properties/CedarCrestTaguig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/taguig-CedarCrest.jpg",
                         "mapPhoto": "app/img/propertiesMap/taguig-CedarCrest.jpg",
                         "address": "Acacia Estates, Taguig City"
                     }, {
                         "propName": "Cypress Tower",
-                        "link": "properties/CypressTowersTaguig.html#mainSection",
+                        "link": "app/properties/CypressTowersTaguig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/taguig-CypressTowers.jpg",
                         "mapPhoto": "app/img/propertiesMap/taguig-CypressTowers.jpg",
                         "address": "C5 Corner Diego Silang St., Taguig City"
                     }, {
                         "propName": "Verawood Residences",
-                        "link": "properties/VerawoodResidencesTaguig.html#mainSection",
+                        "link": "app/properties/VerawoodResidencesTaguig.html#mainSection",
                         "namePhoto": "app/img/propertiesName/taguig-Verawood.jpg",
                         "mapPhoto": "app/img/propertiesMap/taguig-VerawoodResidences.jpg",
                         "address": "Acacia Estates, Taguig City"
@@ -316,7 +321,7 @@ class CityProp extends React.Component {
                             <div className="cityProp">
                                 <DMCILogo />
 
-                                <h2 className="text-left" style={{ background: "url('app/img/icons/locationIcon.png') 10px 5px/25px no-repeat padding-box" }}>{city.cityName} Condo</h2>
+                                <h2 className="text-left" style={{ background: "url('app/img/icons/locationIcon.png') 8px 15px/25px no-repeat padding-box" }}>{city.cityName} Condo</h2>
                                 <hr />
 
                                 {city.lists.map(function(subSec, index){
@@ -336,13 +341,13 @@ class CityProp extends React.Component {
                                                                 <div key={condoLoc.propName} >
                                                                     <h5>{condoLoc.propName}</h5>
                                                                     <div className="propName">
-                                                                        <Link to='/Condos'>
+                                                                        <a target="_blank" href={condoLoc.link}>
                                                                             <img src={condoLoc.namePhoto} alt={condoLoc.propName} className="img-responsive" />
                                                                             <img src={condoLoc.mapPhoto} alt={condoLoc.propName} className="img-responsive" />
                                                                             <br />
                                                                             <p className="infoBtn">More Info</p>
                                                                             <p className="text-left">Location:&nbsp; {condoLoc.address}</p>
-                                                                        </Link>
+                                                                        </a>
                                                                     </div>
                                                                 </div>
                                                             );
